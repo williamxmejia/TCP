3a556407bfb470aa8df33ee338f8620269983237
williamxmejia <willx1995@gmail.com>
2024-12-25 21:17:10 -0600

    completed all the functionality

--- chat/main.go
+++ chat/main.go
@@ -1,28 +1,36 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/template/html/v2"
-	"chat/handlers"
+    "github.com/gofiber/fiber/v2"
+    "github.com/gofiber/template/html/v2"
+    "github.com/gofiber/websocket/v2"
+    "chat/handlers"
 )
 
 func main() {
-
 	viewsEngine := html.New("./views", ".html")
 
-	app := fiber.New(fiber.Config {
+	app := fiber.New(fiber.Config{
 		Views: viewsEngine,
 	})
 
 	app.Static("/static/", "./static")
+	appHandler := handlers.NewAppHandler()
+	server := NewWebSocket()
+
+	// app.Use(server.HandleConnections)
 
 	app.Get("/ping", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("Welcome to fiber")
 	})
 
-	appHandler := handlers.NewAppHandler()
-
 	app.Get("/", appHandler.HandleGetIndex)
+	app.Get("/ws", websocket.New(func(ctx *websocket.Conn) {
+		server.HandleWebSocket(ctx)
+	}))
+
+	go server.HandleMessages()
 
 	app.Listen(":3000")
+
 }
--- chat/messages.go
+++ chat/messages.go
@@ -1,5 +1,7 @@
 package main
 
 type Message struct {
+	ClientName string `json:"client_name"`
 	Text	string `json:"text"`
+
 }
